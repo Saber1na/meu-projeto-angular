e8966dcf5a00fc2be0da6f221f77a9e096b01c63
Sabrina Andrade <146598433+Saber1na@users.noreply.github.com>
2024-11-13 09:21:11 -0300

    Fazendo app-compra

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { PerfilComponent } from './components/perfil/perfil.component';
 import { HeaderComponent } from './components/header/header.component';
 import { InicioComponent } from "./components/inicio/inicio.component";
 import { ProdutosComponent } from "./components/produtos/produtos.component";
+import { CompraComponent } from "./components/compra/compra.component";
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PerfilComponent, HeaderComponent, InicioComponent, ProdutosComponent],
+  imports: [RouterOutlet, PerfilComponent, HeaderComponent, InicioComponent, ProdutosComponent, CompraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
