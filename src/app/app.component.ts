d7a049df09e7084d3c577b5d374d024551c0ce88
Sabrina Andrade <146598433+Saber1na@users.noreply.github.com>
2024-11-21 19:00:54 -0300

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { PerfilComponent } from './components/perfil/perfil.component';
 import { HeaderComponent } from './components/header/header.component';
 import { InicioComponent } from "./components/inicio/inicio.component";
 import { ProdutosComponent } from "./components/produtos/produtos.component";
@@ -11,7 +9,7 @@ import { CompraComponent } from "./components/compra/compra.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PerfilComponent, HeaderComponent, InicioComponent, ProdutosComponent, CompraComponent],
+  imports: [HeaderComponent, InicioComponent, ProdutosComponent, CompraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
