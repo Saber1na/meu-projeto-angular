bd7901cd32d3eb7f96f7725e6587389ad605f10a
Sabrina Andrade <146598433+Saber1na@users.noreply.github.com>
2024-11-21 22:34:42 -0300

    Atualizacao
    App-compra
    App-carrinho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-import { InicioComponent } from "./components/inicio/inicio.component";
-import { ProdutosComponent } from "./components/produtos/produtos.component";
-import { CompraComponent } from "./components/compra/compra.component";
-
-
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { CompraComponent } from './components/compra/compra.component';
+import { CarrinhoComponent } from './components/carrinho/carrinho.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, InicioComponent, ProdutosComponent, CompraComponent],
+  imports: [HeaderComponent, InicioComponent, ProdutosComponent, CompraComponent, CarrinhoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'meu-projeto-angular';
---
+++ src/app/components/carrinho/carrinho.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho',
+  standalone: true,
+  templateUrl: './carrinho.component.html',
+  styleUrls: ['./carrinho.component.css']
+})
+export class CarrinhoComponent {
+
+}
