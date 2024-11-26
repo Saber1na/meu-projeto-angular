80558172c2ace41d28a78082310aaea1c723bcae
Victor Fernandes <154020731+VFSomente@users.noreply.github.com>
2024-11-25 22:16:41 -0300

    Atulização 1.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './components/header/header.component';
-import { InicioComponent } from './components/inicio/inicio.component';
-import { ProdutosComponent } from './components/produtos/produtos.component';
-import { CompraComponent } from './components/compra/compra.component';
-import { CarrinhoComponent } from './components/carrinho/carrinho.component';
-import { SobreComponent } from "./components/sobre/sobre.component";
-import { ContatoComponent } from "./components/contato/contato.component";
-import { LoginComponent } from "./components/login/login.component";
-import { FooterComponent } from "./components/footer/footer.component";
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, InicioComponent, ProdutosComponent, CompraComponent, CarrinhoComponent, SobreComponent, ContatoComponent, LoginComponent, FooterComponent],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
